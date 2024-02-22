c49e16b19b05588d7f8bbfbc7d5558f9830775b2
Michael Lessard <michael.lessard@gmail.com>
2024-02-22 21:16:59 +0000

    last update

--- chat-chainlint.py
+++ chat-chainlint.py
@@ -1,34 +1,38 @@
-from typing import List 
+import chainlit as cl
 from ctransformers import AutoModelForCausalLM
 
-llm = AutoModelForCausalLM.from_pretrained(
-    "zoltanctoth/orca_mini_3B-GGUF", model_file="orca-mini-3b.q4_0.gguf"
-)
-
-
-def get_prompt(instruction: str, history: list[str] | None = None) -> str:
+def get_prompt(instruction: str, history: list[str]) -> str:
     system = "You are an AI assistant that gives helpful answers. You answer the question in a short and concise way."
     prompt = f"### System:\n{system}\n\n### User:\n"
-    if history is not None:
+    if len(history) > 0:
         prompt += f"This is the conversation history: {''.join(history)}. Now answer the question: "
     prompt += f"{instruction}\n\n### Response:\n"
     print(f"Prompt created: {prompt}")
     return prompt
 
 
-history = []
+@cl.on_chat_start
+async def on_chat_start():
+    global llm
+    llm = AutoModelForCausalLM.from_pretrained(
+        "zoltanctoth/orca_mini_3B-GGUF", model_file="orca-mini-3b.q4_0.gguf"
+    )
 
-question = "Which city is the capital of India?"
-prompt = get_prompt(question)
-answer = ""
-for word in llm(prompt, stream=True):
-    print(word, end="", flush=True)
-    answer += word
-print()
-history.append(answer)
+    cl.user_session.set("message_history", [])
 
-question = "And of the United States?"
-prompt = get_prompt(question, history)
-for word in llm(prompt, stream=True):
-    print(word, end="", flush=True)
-print()
+    await cl.Message("Model initialized. How can I help you?").send()
+
+@cl.on_message
+async def on_message(message: cl.Message):
+    message_history = cl.user_session.get("message_history")
+    msg = cl.Message(content="")
+    await msg.send()
+
+    prompt = get_prompt(message.content, message_history)
+    response = ""
+    for word in llm(prompt, stream=True):
+        await msg.stream_token(word)
+        response += word
+    await msg.update()
+    message_history.append(response)
+
